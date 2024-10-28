156245280ca27221bf283eb9f8baf897cd9867c0
Anas <anasginolt@gmail.com>
2024-10-29 04:47:04 +0530

    Initial commit of hackathon project with API and client

---
+++ api/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const bodyParser = require("body-parser")
+
+const authRoute = require("./routes/auth")
+
+
+const cors = require("cors");
+
+const app = express();
+const port = 5000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+dotenv.config();
+
+async function connectDB() {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("DB Connection successfull");
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+connectDB();
+
+app.use(cors())
+app.use("/api/auth", authRoute);
+app.listen(port, () => {
+    console.log(`Server is running on port:${port}`);
+  }); 
